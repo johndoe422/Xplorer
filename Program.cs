9fad0df2f9999f3adc27197fc9ea18e294082ede
johndoe422 <30309651+johndoe422@users.noreply.github.com>
2025-03-28 22:27:22 +0530

    Prevented multiple instances

--- Program.cs
+++ Program.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Xplorer
 {
     static class Program
     {
+        private static Mutex mutex = null;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            const string appName = "03817d52-b2bd-433d-b207-751fea6737d6";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("An instance of this application is already running.");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
