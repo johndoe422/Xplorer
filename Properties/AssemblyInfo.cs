655eaf3a310606cbde0c1cb8a1817f9f25ca9af7
johndoe422 <30309651+johndoe422@users.noreply.github.com>
2025-03-29 15:40:02 +0530

    Changes to assembly info for auto update preparations

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Xplorer")]
-[assembly: AssemblyDescription("Win 10 Quick Launch equivalent for My Computer")]
+[assembly: AssemblyDescription("A file explorer as an expanding menu of subfolders")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sunil TG")]
 [assembly: AssemblyProduct("Xplorer")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2")]
